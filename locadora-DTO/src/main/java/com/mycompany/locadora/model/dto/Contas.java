8a2edceeaf339fe049843a8a32586c06bc7f64f2
Aluno <Aluno@DECOM-LAB-040>
2022-10-10 10:20:50 -0300

    Inicio da criação das classes principais do modelo MVC #1

---
+++ locadora-DTO/src/main/java/com/mycompany/locadora/model/dto/Contas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.locadora.model.dto;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Contas {
+    public double valor;
+    public String remetente;
+    public String destinatario;
+}
---
+++ locadora-DTO/src/main/java/com/mycompany/locadora/model/dto/Funcionario.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.locadora.model.dto;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Funcionario extends Usuario{
+    private double salario;
+    
+    public Funcionario(String username, String senha, double salario){
+        this.username = username;
+        this.senha = senha;
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+}
