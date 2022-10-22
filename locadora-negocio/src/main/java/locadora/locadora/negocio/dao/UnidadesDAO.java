f6a8bd4a3fd85d8be7cd52ba47090570a2d1fa29
sufra <sufra@192.168.0.111>
2022-10-22 20:37:51 -0300

    inicio da criacao das classes DAO e de Persistencia de unidades #14 #15

--- locadora-negocio/src/main/java/locadora/locadora/negocio/dao/UnidadesDAO.java
+++ locadora-negocio/src/main/java/locadora/locadora/negocio/dao/UnidadesDAO.java
@@ -8,7 +8,74 @@ import locadora.locadora.negocio.dto.Unidades;
 import java.util.List;
 
 public class UnidadesDAO {
-    private static void cadastrarUnidadesBD(Unidades u){}
-    private static Unidades removerUnidadeBD(Unidades u){return null;}
-    private static List<Unidades> listarUnidadesBD(){return null;}
+    
+    public static Unidades consultarPorCep(Integer cep) {
+        if (listarUnidadesBD() != null) {
+            for (Unidades u : listarUnidadesBD()) {
+                if (u.getCep().equals(cep)) {
+                    return u;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static Unidades consultarPorEndereco(String endereco) {
+        if (listarUnidadesBD() != null) {
+            for (Unidades u : listarUnidadesBD()) {
+                if (u.getEndereco().equals(endereco)) {
+                    return u;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static Unidades consultarPorCodigo(Integer codigo) {
+        if (listarUnidadesBD() != null) {
+            for (Unidades u : listarUnidadesBD()) {
+                if (u.getCodigo().equals(codigo)) {
+                    return u;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static Unidades consultarPorNumReferencia(Integer numReferencia) {
+        if (listarUnidadesBD() != null) {
+            for (Unidades u : listarUnidadesBD()) {
+                if (u.getNumReferencia().equals(numReferencia)) {
+                    return u;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Unidades cadastrarUnidadeBD(String endereco, Integer numReferencia, Integer codigo, Integer cep) {
+        if (consultarPorCep(cep) == null | listarUnidadesBD() == null) {
+            Unidades unidade = new Unidades(endereco, numReferencia, codigo, cep);
+            // adicionar unidade ao banco de dados (REALIZADO FUTURAMENTE)
+            return unidade;
+        }
+        return null;
+    }
+
+    public static Unidades removerUnidadeBD(Integer cep) {
+        if (consultarPorCep(cep) != null) {
+            Unidades unidade = consultarPorCep(cep);
+            // apagar veiculo do banco de dados (REALIZADO FUTURAMENTE)
+            return unidade;
+        }
+        return null;
+    }
+
+    public static List<Unidades> listarUnidadesBD(){
+        /* PARTE DO BANCO DE DADOS (RETORNA LISTA DOS VEICULOS) if (contatoList.isEmpty() != true) {
+            return contatoList;
+        }
+         */
+        return null;
+    }
 }
